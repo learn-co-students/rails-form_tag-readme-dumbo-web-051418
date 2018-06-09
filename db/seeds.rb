13eacdfce7f054283594bb941b8b40ad3be82888
Malorie <malorie18@gmail.com>
2018-06-09 16:29:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "Real lyfe", description: "I'm searching for that real life")
